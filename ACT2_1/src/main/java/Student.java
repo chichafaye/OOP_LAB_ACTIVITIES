9e712b96746c15b7adafa289c400d6206de4e47b
chichafaye <160128270+chichafaye@users.noreply.github.com>
2024-03-06 21:17:52 +0800

    Add files via upload

---
+++ ACT2_1/src/main/java/Student.java
@@ -0,0 +1,30 @@
+
+package Encapsulation;
+
+public class Student {
+    private String firstName,lastName;
+    private int age;
+    private boolean isMinor;
+
+    public Student(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.isMinor = age<18;
+    }
+    
+public String getName() {
+        return lastName + ", " + firstName;
+    }
+
+    public void increaseAge() {
+        age++;
+        isMinor = age < 18;
+    }
+    
+    @Override
+    public String toString() {
+        String status = isMinor ? "minor" : "adult";
+        return getName() + " - " + age + " - " + status;
+    }
+}
